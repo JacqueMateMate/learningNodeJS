63a09cbdab49ec473a4ca024e3ad84d8e39d5247
JacqueMateMate <monta.johana@gmail.com>
2019-07-16 12:26:39 +0200

    updated files

--- javascripting/makeitmodular.js
+++ javascripting/makeitmodular.js
@@ -1,11 +1,9 @@
 const myModule = require('./filteredls.js')
 var path = process.argv[2]
 var extension = process.argv[3]
- myModule(path,extension, (err,files)=>{
-    if(err)
+myModule(path, extension, (err, files) => {
+    if (err)
         console.log(err)
-    else{
-       files.forEach(file=>{
-        console.log(file)})}
-    
+    else {
+    }
 })
